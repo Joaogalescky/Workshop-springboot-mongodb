2aac3a5e05c54f73b57e0dba181a688bd147d767
Joaogalescky <joaocvgalescky@gmail.com>
2024-04-24 17:07:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/joaogalescky/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.joaogalescky.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaogalescky.workshopmongo.domain.Post;
+import com.joaogalescky.workshopmongo.repository.PostRepository;
+import com.joaogalescky.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
